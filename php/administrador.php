3790b8baddc58a7bb1e56273b7ebfa561930526c
KennethDom <102487889+KennethDom@users.noreply.github.com>
2022-06-15 21:32:24 -0600

    Botones Administrador
    Se agregaron botones al administrador

--- php/administrador.php
+++ php/administrador.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="../imagenes/iconopes.png">
+    <script type="text/javascript" src=../scripts/funciones.js></script>
     <link rel="stylesheet" href="../css/admin.css">
     <title>BiblioTECa</title>
 </head>
@@ -13,58 +14,14 @@
     <h2>Bienvenido Administrador</h2>
     <p>Selecciona la opción que deseas realizar</p>
     <br><br>
-    <div class="container">
-        <div class="container-image">
-            <a href="#"> <img src = "../imagenes/libro_agregar.jpg" alt=""> </a>
-            <div class="content">
-                <p>
-                    <span>Agregar Libros</span>
-                    El panel Agregar Libros le permite añadir libros a la base de datos
-                </p>
-            </div>
-        </div>
+    <button id="btn" onclick="reporte_libros()">Reporte De Libros</buttton>
+    <button id="btn" onclick="reporte_usuarios()">Reporte De Usuarios</buttton>
+    <button id="btn" onclick="agregar_libro()">Agregar Libro</buttton>
+    <button id="btn" onclick="borrar_libro()">Borrar Libros</buttton>
 
-        
-        <div class="container-image">
-            <a href="#"> <img src = "../imagenes/libro_eliminar.jpg" alt=""> </a>
-            <div class="content">
-                <p>
-                    <span>Eliminar Libros</span>
-                    El panel Eliminar Libros le permite borrar los libros disponibles en la base de datos
-                </p>
-            </div>
-        </div>
-
-        <div class="container-image">
-            <a href="#"> <img src = "../imagenes/libro_report.jpg" alt=""> </a>
-            <div class="content">
-                <p>
-                    <span>Reporte de Libros</span>
-                    El panel Reporte de Libros le permite mostrar en pdf los libros disponibles en la base de datos
-                </p>
-            </div>
-        </div>
-
-        <div class="container-image">
-            <a href="#"> <img src = "../imagenes/user_report.jpg" alt=""></a>
-            <div class="content">
-                <p>
-                    <span>Reporte de Usuarios</span>
-                    El panel Reporte de Usuarios le permite mostrar en pdf los usuarios disponibles en la base de datos
-                </p>
-            </div>
-        </div>
-
-    </div>
 
 <?php
     require "footer.php";
-?>
-
-<?php
-    function borra(){
-        echo 'HOla';
-    }
 ?>
 
 </body>
---
+++ scripts/funciones.js
@@ -0,0 +1,27 @@
+function reporte_libros(){
+    var w = window.alert('','','width=500,height=500');
+    w.document.write('Se esta generando tu reporte de libros, por favor espera')
+    setTimeout(function() {w.close();}, 2000)
+    location.href="reporte_libros.php";
+}
+
+function reporte_usuarios(){
+    var w = window.open('','','width=500,height=500');
+    w.document.write('Se esta generando tu reporte de usuarios, por favor espera')
+    setTimeout(function() {w.close();}, 2000)
+    location.href="reporte_usuarios.php";
+}
+
+function agregar_libro(){
+    var w = window.open('','','width=500,height=500');
+    w.document.write('Se te esta redireccionando a la ventana para agregar libros, por favor espera')
+    setTimeout(function() {w.close();}, 2000)
+    location.href="#";
+}
+
+function borrar_libro(){
+    var w = window.open('','','width=500,height=500');
+    w.document.write('Se te esta redireccionando a la ventana para borrar libros, por favor espera')
+    setTimeout(function() {w.close();}, 2000)
+    location.href="#";
+}
