3719170a788bea63421690b8a9dffd1c69ed3c6d
KennethDom <102487889+KennethDom@users.noreply.github.com>
2022-06-15 22:22:15 -0600

    Cambios a los estilos de los botones
    Terminado el diseño de los botones

--- php/administrador.php
+++ php/administrador.php
@@ -6,20 +6,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="../imagenes/iconopes.png">
     <script type="text/javascript" src=../scripts/funciones.js></script>
-    <link rel="stylesheet" href="../css/admin.css">
+    <link rel="stylesheet" href="../css/botones.css">
     <title>BiblioTECa</title>
 </head>
 <body>
+    <div class ="contenedor">
+        <h2>Bienvenido Administrador</h2>
+        <p>Selecciona la opción que deseas realizar</p>
+        <br><br>
 
-    <h2>Bienvenido Administrador</h2>
-    <p>Selecciona la opción que deseas realizar</p>
-    <br><br>
-    <button id="btn" onclick="reporte_libros()">Reporte De Libros</buttton>
-    <button id="btn" onclick="reporte_usuarios()">Reporte De Usuarios</buttton>
-    <button id="btn" onclick="agregar_libro()">Agregar Libro</buttton>
-    <button id="btn" onclick="borrar_libro()">Borrar Libros</buttton>
-
-
+        <div class="contenedor_botones">
+            <button class="btn" onclick="reporte_libros()"><span>Reporte De Libros</span></buttton>
+            <button class="btn" onclick="reporte_usuarios()"><span>Reporte De Usuarios</span></buttton>
+            <button class="btn" onclick="agregar_libro()"><span>Agregar Libro</span></buttton>
+            <button class="btn" onclick="borrar_libro()"><span>Borrar Libro</span></buttton>
+        </div>
+    </div>
 <?php
     require "footer.php";
 ?>
